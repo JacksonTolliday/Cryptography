b991db94a01eb6dbf6516b983e31c5045d276124
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-10-03 11:36:35 -0400

    Updated from Brython Server: 10/3/2018 11:36:34 AM

--- cryptography.py
+++ cryptography.py
@@ -16,6 +16,33 @@ associationsnu = list(range(len(associationsn)))
 letternumzip = list(zip(associationsnu, associationsn))
 print(letternumzip)
 '''
+def scramble(message): #, key, direction
+    for x,y in letternumzip:
+        c = 0
+        if c in list(message) == y:
+            leftover = range(len(message))
+            message.append(x)
+            c = c+1
+print(scramble('Hello, World!'))
+'''
+def numbs(message): #, key, direction
+    swap = 1
+    while swap > 0:
+        swap = 0
+        for x,y in letternumzip:
+            c = 0
+            cmax = len(message)-1
+            if c < cmax:
+                if c in list(message) == y:
+                    if ltrzip[c][0] < ltrzip[c+1][0]:
+                        leftover = range(len(message))
+                        message.append(x)
+                        del message(0)
+                        c = c+1
+                        swap = 1
+
+
+'''
 dore = input(str('Enter e to encrypt, d to decrypt, or q to quit: '))
 run = True
 rerun = False
